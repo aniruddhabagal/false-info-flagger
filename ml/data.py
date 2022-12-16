09c7994818ea3dc9055c1483319cc0616903d85e
SahilJain8 <jainsahil1116@gmail.com>
2022-12-16 14:07:59 +0530

    data update

--- ml/data.py
+++ ml/data.py
@@ -6,21 +6,27 @@ json_dict={}
 
 class data_clean:
     def data_clean():
-        train_df=pd.read_csv("ml\\train_data_final.csv")
+        train_df=pd.read_csv("Data\\train_data_final.csv")
         i=0
         for act_name in train_df["ACT_NAME"]:
+            
             act_nam=train_df["ACT_NAME"][i]
             act_id=train_df["ACT_ID"][i]
             try:
+                print(i)
+                i+=1
                 data=wikipedia.summary(act_name,sentences=10)
                 data=re.sub("\n","",data)
                 data=re.sub("==","",data)
-
+                
                 json_dict[i]=[{"act_name":act_nam},{"act_id":act_id},{"act_data":data}]
+               
+                with open('final.json', 'w') as fp:
+                                json.dump(json_dict, fp,  indent=4)
+                
             except Exception as e:
-                pass
-            i+=1
-            
+                print(str(e))
+
 
 obj=data_clean.data_clean()
 
