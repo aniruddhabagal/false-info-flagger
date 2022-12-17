8adb635827b6aa3a1b0ee5bdb3565a3142feb470
Aniruddha Bagal <bagalaniruddha@gmail.com>
2022-12-17 10:41:03 +0530

    Added Final Extension v1

---
+++ extension/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import logo from "./assets/falsify.png";
+
+function url() {
+  const url = window.location.href;
+  return url;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="logo" width="160px" />
+      <h1>
+        <span id="cap">Capture</span> <span>The Flag</span>{" "}
+      </h1>
+      <button>Check for Flags</button>
+      <br />
+      <p>We help you capture the FLAG! </p>
+      <br />
+    </div>
+  );
+}
+
+export default App;
